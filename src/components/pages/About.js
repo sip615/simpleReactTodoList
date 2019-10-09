a213916d5df319be472562fe491f09c4e4d95da5
sip615 <sunilpatel.328@gmail.com>
2019-10-09 12:26:22 -0400

    commiting these files from a different local repo into ths one

---
+++ src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function About() {
+	return (
+		<React.Fragment>
+			<h1>About</h1>
+			<p>
+				This is the Todo app version 1.0.0 It is part of a React practice
+				program
+			</p>
+		</React.Fragment>
+	);
+}
+
+export default About;
